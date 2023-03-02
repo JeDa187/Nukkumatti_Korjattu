519be3413614211620d0342821aeb3516f6c4362
JeDa187 <j.saarela@dr.com>
2023-03-02 11:33:15 +0200

    Enemy attack korjattu

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class EnemyAttack : MonoBehaviour
 {
 
     public Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+  
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
@@ -22,7 +20,20 @@ public class EnemyAttack : MonoBehaviour
 
 
         }
-
-
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        animator.SetTrigger("EnemyAttack");
+    //    }
+    //}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            animator.SetTrigger("EnemyAttack");
+    }
+
 }
